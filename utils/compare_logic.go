dfdd5538b6e300651f26edb18555304934a86591
sdjqwbz_work <sdjqwbzWork@163.com>
2024-10-16 18:48:46 +0800

    mod: 比较逻辑添加泛型定义、移除MustNil()、限定Must()入参为error类型

--- utils/compare_logic.go
+++ utils/compare_logic.go
@@ -4,38 +4,32 @@ import (
 	"strings"
 )
 
-// Must 必须不为Nil
-func Must(in any, msg ...string) {
-	if inE, ok := in.(error); ok {
-		msg = append(msg, inE.Error())
-	}
+type integerNumber interface {
+	int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64
+}
+
+// Must 必须无错误 否则Panic
+func Must(in error, msg ...string) {
+	msg = append(msg, in.Error())
 	panicIfCondition(in != nil, msg...)
 }
 
-// MustNil 必须为Nil
-func MustNil(in any, msg ...string) {
-	if inE, ok := in.(error); ok {
-		msg = append(msg, inE.Error())
-	}
-	panicIfCondition(in == nil, msg...)
-}
-
-// MustZero 必须为0
-func MustZero(in int, msg ...string) {
+// MustZero 必须为0 否则Panic
+func MustZero[T integerNumber](in T, msg ...string) {
 	panicIfCondition(in == 0, msg...)
 }
 
-// MustZeroN 必须不为0
-func MustZeroN(in int, msg ...string) {
+// MustZeroN 必须不为0 否则Panic
+func MustZeroN[T integerNumber](in T, msg ...string) {
 	panicIfCondition(in != 0, msg...)
 }
 
-// MustTrue 必须为true
+// MustTrue 必须为True 否则Panic
 func MustTrue(in bool, msg ...string) {
 	panicIfCondition(in, msg...)
 }
 
-// MustFalse 必须为false
+// MustFalse 必须为False 否则Panic
 func MustFalse(in bool, msg ...string) {
 	panicIfCondition(!in, msg...)
 }
